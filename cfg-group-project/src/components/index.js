b2bc1ac7309a019c6e7c33812a1dd71e2fcc9de7
Anca David <94863926+anca-david@users.noreply.github.com>
2023-03-29 22:34:51 +0100

    Delete cfg-group-project directory

--- cfg-group-project/src/components/index.js
+++
@@ -1,2 +0,0 @@
-export {default as Button} from './button/Button';
-export {default as Feature} from './feature/Feature';
--- cfg-group-project/src/containers/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Blog } from './blog/Blog';
-export { default as Navbar } from './navbar/Navbar';
-export { default as Footer } from './footer/Footer';

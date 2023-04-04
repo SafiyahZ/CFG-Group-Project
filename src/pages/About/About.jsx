4ea4284681250758b652a112165511920a901345
Anju-Lama <Anju.lama@hotmail.com>
2023-04-04 21:48:29 +0100

    Added text within abouts page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,13 +1,29 @@
 import React from "react";
-import './about.css';
+import "./about.css";
 
-function About () {
-    return (
-        <div>
-        <h1>About page</h1>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde, sapiente!</p>
-      </div>
-    );
+function About() {
+  return (
+    <div>
+      <h1>ABOUT US...</h1>
+      <p>
+        We're thrilled that you're here to join us in celebrating our love for
+        cats and dogs. Our goal is to provide a fun and engaging platform where
+        you can come and see photos of different types of pets, ranging from
+        cute to funny.
+      </p>
+      <p>
+        We believe that there are many benefits to visually seeing cats and dogs
+        photos. It can help promote relaxation and reduce stress levels, boost
+        mood, foster creativity, and provide entertainment. We hope that our
+        photo gallery can be a source of positivity and joy for our visitors.
+      </p>
+      <p>
+        Thank you for visiting our pet photo gallery, and we hope that you enjoy
+        browsing through our collection of adorable pet photos.
+      </p>
+      <h2>MEET THE TEAM</h2>
+    </div>
+  );
 }
 
-export default About
+export default About;

0d1becb01f343e17b13a319b04a6844e0034ef54
Anca David <94863926+anca-david@users.noreply.github.com>
2023-04-10 02:09:44 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -33,34 +33,3 @@ function App() {
 export default App;
 
 
-// import React from 'react';
-// import { Navbar, Footer } from './components';
-// import './App.css';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import Home from './pages/Home/Home';
-// import About from './pages/About/About';
-// import Dogs from './pages/Dogs/Dogs';
-// import Dog from './pages/Dog/Dog'; // Imported Dog component
-// import Cats from './pages/Cats/Cats';
-// import Cat from './pages/Cat/Cat'; // Imported Cat component
-// import Contact from './pages/Contact/Contact';
-
-// function App() {
-//   return (
-//     <Router>
-//       <Navbar />   
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/About" element={<About />} />
-//         <Route path="/dogs" element={<Dogs />} />
-//         <Route path="/:dogName" element={<Dog />} /> 
-//         <Route path="/cats" element={<Cats />} />
-//         <Route path="/:catName" element={<Cat />} /> 
-//         <Route path="/Contact" element={<Contact />} />
-//       </Routes>
-//       <Footer />
-//     </Router>
-//   );
-// }
-
-// export default App;

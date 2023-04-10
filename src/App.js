3d1323d7acd2aef9d13624d52a2ae95be8f1efa5
Anca David <94863926+anca-david@users.noreply.github.com>
2023-04-10 01:50:00 +0100

    cat_pics

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import './App.css';
 import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
-import Pets from './pages/Pets/Pets';
+import Dogs from './pages/Dogs/Dogs';
 import Dog from './pages/Dog/Dog';
+import Cats from './pages/Cats/Cats';
+import Cat from './pages/Cat/Cat';
 import Contact from './pages/Contact/Contact';
+import RandomCat from './pages/RandomCat/RandomCat';
 
 function App() {
   return (
@@ -16,8 +19,11 @@ function App() {
         <Route path="/" exact element={<Home />} />
         <Route path="/About" element={<About />} />
         <Route path="/:name" element={<Dog />} />
-        <Route path="/Pets" element={<Pets />} />
+        <Route path="/Dogs" element={<Dogs />} />
+        <Route path="/:name" element={<Cat />} />
+        <Route path="/Cats" element={<Cats />} />
         <Route path="/Contact" element={<Contact />} />
+        <Route path="/RandomCat" element={<RandomCat />} />
       </Routes>
       <Footer />
     </Router>
@@ -27,3 +33,34 @@ function App() {
 export default App;
 
 
+// import React from 'react';
+// import { Navbar, Footer } from './components';
+// import './App.css';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import Home from './pages/Home/Home';
+// import About from './pages/About/About';
+// import Dogs from './pages/Dogs/Dogs';
+// import Dog from './pages/Dog/Dog'; // Imported Dog component
+// import Cats from './pages/Cats/Cats';
+// import Cat from './pages/Cat/Cat'; // Imported Cat component
+// import Contact from './pages/Contact/Contact';
+
+// function App() {
+//   return (
+//     <Router>
+//       <Navbar />   
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/About" element={<About />} />
+//         <Route path="/dogs" element={<Dogs />} />
+//         <Route path="/:dogName" element={<Dog />} /> 
+//         <Route path="/cats" element={<Cats />} />
+//         <Route path="/:catName" element={<Cat />} /> 
+//         <Route path="/Contact" element={<Contact />} />
+//       </Routes>
+//       <Footer />
+//     </Router>
+//   );
+// }
+
+// export default App;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,8 @@
-export {default as Pets} from './Pets/Pets';
+export {default as Dogs} from './Dogs/Dogs';
 export {default as About } from './About/About';
 export {default as Home} from './Home/Home';
 export {default as Contact} from './Contact/Contact';
 export {default as Dog} from './Dog/Dog';
+export {default as Cats} from './Cats/Cats';
+export {default as Cat} from './Cat/Cat';
+export {default as RandomCat} from './RandomCat/RandomCat';

40bfda98efc6bbf78ce0dd12cdc6359656bbc0d9
Anca David <94863926+anca-david@users.noreply.github.com>
2023-04-10 02:47:08 +0100

    app_fix

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,48 +1,62 @@
 import React from "react";
-import './about.css';
-import { Link } from 'react-router-dom';
+import "./about.css";
+// import { Link } from "react-router-dom";
+import Anca from  '../../assets/Anca.png';
+import anju from '../../assets/anju.png';
+import Megs from '../../assets/Megs.png';
+import Sade from '../../assets/Sade.png';
+import Safiyah from  '../../assets/Safiyah.png';
 
-function clickMe(){
-  alert ('You clicked me :3')
-}
-
+// function clickMe() {
+//   alert("You clicked me :3");
+// }
 
 function About () {
     return (
-        <div>
-        <h1>About Us</h1>
-        <h2>SAAMS</h2>
-        <p>
+      <div>
+      {/* <h1 className='title2'> About Us</h1> */}
+    <div className="about">
+      <h2 className='title3'>SAAMS</h2>
+      <p className="about_p">
         We're thrilled that you're here to join us in celebrating our love for
         cats and dogs. Our goal is to provide a fun and engaging platform where
         you can come and see photos of different types of pets, ranging from
         cute to funny.
       </p>
-      <p>
+      <p className="about_p">
         We believe that there are many benefits to visually seeing cats and dogs
         photos. It can help promote relaxation and reduce stress levels, boost
         mood, foster creativity, and provide entertainment. We hope that our
         photo gallery can be a source of positivity and joy for our visitors.
       </p>
-      <p>
+      <p className="about_p">
         Thank you for visiting our pet photo gallery, and we hope that you enjoy
         browsing through our collection of adorable pet photos.
       </p>
 
-      <h3>MEET THE TEAM</h3>
+      <h3 className='teaminfo'> MEET THE TEAM</h3>
 
-<p>Our team, SAAMS is made up of devoted pet owners and animal lovers who are dedicated to promoting responsible pet ownership, doses of serotonin, providing
-resources and support to pet owners. We hope these photos not only give you a boost of serotonin but persuades you to look into adopting rather than purchasing from puppy mills. 
-We believe that every pet deserves a loving fur-ever home!</p>
+      <p className="about_p">
+        Our team, SAAMS is made up of devoted pet owners and animal lovers who
+        are dedicated to promoting responsible pet ownership, doses of
+        serotonin, providing resources and support to pet owners. We hope these
+        photos not only give you a boost of serotonin but persuades you to look
+        into adopting rather than purchasing from puppy mills. We believe that
+        every pet deserves a loving fur-ever home!
+      </p>
+    </div>
+      {/* <button onClick={clickMe}>
+        <Link to="/Pets">View Pets!</Link>
+      </button> */}
+      <div className="Container-for-team-photo">
+        <img src={Anca} alt="photo of Anca" className="team-photo" />
+        <img src={anju} alt="photo of anju" className="team-photo" />
+        <img src={Megs} alt="photo of Megs" className="team-photo" />
+        <img src={Sade} alt="photo of Sade" className="team-photo" />
+        <img src={Safiyah} alt="photo of Safiyah" className="team-photo" />
+      </div>
+    </div>
+  );
+}
 
-
-<button onClick={clickMe}>
-<Link to="/Pets">View Pets!</Link>
-  </button> 
-
-</div>
-    );
-};
-
-
-export default About
+export default About;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -14,8 +14,8 @@ function Home() {
         <div className="info">
           <h3 className="title">What Do We Do?</h3>
           <h4 className="description">Welcome to Pawsitive Vibes, your daily dose of happiness! We know life can be stressful, but sometimes all you need is a little break to recharge and feel better. That's where we come in. Our website is filled with adorable animals that are guaranteed to put a smile on your face and help you unwind. From fluffy puppies to curious kittens, we've got it all. So take a deep breath, relax, and let our furry friends bring you some much-needed joy.</h4>
-          <a href="/pets">
-            <button className="viewpets">View Pets</button>
+          <a href="/Dogs">
+            <button className="viewpets">Explore</button>
           </a>
         </div>
         <img src={reviews} alt="pets" className="review-image" />
